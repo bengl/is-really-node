e996fea95a28077b4b3a7a3c1287b5d934f1062b
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2023-09-26 22:19:16 -0500

    Update index.d.ts
    Co-authored-by: Bryan English <bryan@bryanenglish.com>

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export default isReallyNode;
 /**
- * Returns true if we're definitely running inside Node.js.
+ * True if we're definitely running inside Node.js.
  */
 declare const isReallyNode: boolean;
